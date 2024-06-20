719d599af17b63206831ef95dd1d27700093581a
Nakul Chouksey <71.nakul@gmail.com>
2024-06-20 17:02:05 +0530

    dark mode done✅

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,14 @@ import React, { useContext } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
+import { FaCaretDown, FaPencilAlt, FaRegCompass } from "react-icons/fa";
+import { RiBallPenFill } from "react-icons/ri";
+import { AiFillCompass } from "react-icons/ai";
+import { MdLightbulbOutline } from "react-icons/md";
+import { SiGooglemaps } from "react-icons/si";
+import { TbBrandGoogleMaps } from "react-icons/tb";
+import { BiImageAdd } from "react-icons/bi";
+import { IoIosCamera, IoMdSend } from "react-icons/io";
 
 const Main = () => {
   const {
@@ -31,9 +39,9 @@ const Main = () => {
   };
 
   return (
-    <div className="main flex-[1] min-h-[100vh] pb-[15vh] relative">
-      <div className="nav flex items-center justify-between text-[20px] p-[20px] text-[#585858]">
-        <p>Gemini</p>
+    <div className="main bg-[#131314] flex-[1] min-h-[100vh] pb-[15vh] relative">
+      <div className="nav flex items-center justify-between text-[20px] p-[20px] text-[#CACCCE]">
+        <p className="font-normal flex items-center">Gemini <FaCaretDown /></p>
         <img
           src={assets.user_icon}
           alt=""
@@ -41,113 +49,103 @@ const Main = () => {
         />
       </div>
       <div className="main-container max-w-[900px] m-auto">
+        {!showResult ? (
+          <>
+            <div className="greet tracking-tighter my-50px text-[56px] text-[#c4c7c5] font-medium p-[20px]">
+              <p>
+                <span>Hello, Nakul</span>
+              </p>
+              <p className="text-[#444746]">How can I help you today?</p>
+            </div>
+            <div className="cards sm:hidden">
+              <div className="card hover:bg-[#333537] h-[200px] p-[15px] bg-[#1E1F20] rounded-[10px] relative cursor-pointer">
+                <p className="text-[#E3E3E3] font-[17px]">
+                Write a product description for a new type of toothbrush
+                </p>
+                <RiBallPenFill className="p-[8px] w-[38px] h-[38px] absolute rounded-full text-[#E3E3E3] bg-zinc-900 bottom-[15px] right-[15px]" />
+              </div>
+              <div className="card hover:bg-[#333537] h-[200px] p-[15px] bg-[#1E1F20] rounded-[10px] relative cursor-pointer">
+                <p className="text-[#E3E3E3] font-[17px]">
+                Create a travel itinerary for a city
+                </p>
+                <FaRegCompass className="p-[8px] w-[38px] h-[38px] absolute rounded-full text-[#E3E3E3] bg-zinc-900 bottom-[15px] right-[15px]" />
+              </div>
+              <div className="card hover:bg-[#333537] h-[200px] p-[15px] bg-[#1E1F20] rounded-[10px] relative cursor-pointer">
+                <p className="text-[#E3E3E3] font-[17px]">
+                Help me compare these college majors
+                </p>
+                <MdLightbulbOutline className="p-[5px] w-[38px] h-[38px] absolute rounded-full text-[#E3E3E3] bg-zinc-900 bottom-[15px] right-[15px]" />
+              </div>
+              <div className="card hover:bg-[#333537] h-[200px] p-[15px] bg-[#1E1F20] rounded-[10px] relative cursor-pointer">
+                <p className="text-[#E3E3E3] font-[17px]">
+                What's the time it takes to walk to several landmarks
+                </p>
+                <SiGooglemaps className="p-[8px] w-[38px] h-[38px] absolute rounded-full text-[#E3E3E3] bg-zinc-900 bottom-[15px] right-[15px]" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result px-[5%] max-h-[70vh] overflow-y-scroll scrollbar-hide">
+            <div className="result-title font-medium text-[17px] my-[40px] flex items-center gap-[26px]">
+              <img
+                className="w-[40px] h-[40px] rounded-full object-cover"
+                src={assets.user_icon}
+                alt=""
+              />
+              <p className="text-[#E3E3E3]">{recentPrompt}</p>
+            </div>
+            <div className="result-data flex items-start gap-[20px]">
+              <img className="w-[45px]" src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader w-full flex flex-col gap-[10px]">
+                  <hr className="rounded-[4px] border-0 bg-[#f6f7f8] h-[20px]" />
+                  <hr className="rounded-[4px] border-0 bg-[#f6f7f8] h-[20px]" />
+                  <hr className="rounded-[4px] border-0 bg-[#f6f7f8] h-[20px] w-60" />
+                </div>
+              ) : (
+                <p
+                  className="text-[#E3E3E3] text-[17px]"
+                  dangerouslySetInnerHTML={{ __html: resultData }}
+                ></p>
+              )}
+            </div>
+          </div>
+        )}
 
-        {!showResult ? <>
-        
-          <div className="greet my-50px text-[56px] text-[#c4c7c5] font-medium p-[20px]">
-          <p>
-            <span>Hello, Nakul</span>
-          </p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card hover:bg-[#dfe4ea] h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer">
-            <p className="text-[#585858] font-[17px]">
-              Help write SQL to generate a report
-            </p>
-            <img
-              src={assets.compass_icon}
-              alt=""
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-            />
-          </div>
-          <div className="card hover:bg-[#dfe4ea] h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer">
-            <p className="text-[#585858] font-[17px]">
-              Give me phrases to learn a new language
-            </p>
-            <img
-              src={assets.bulb_icon}
-              alt=""
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-            />
-          </div>
-          <div className="card hover:bg-[#dfe4ea] h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer">
-            <p className="text-[#585858] font-[17px]">
-              I’m sick and need help crafting a text message for my boss
-            </p>
-            <img
-              src={assets.message_icon}
-              alt=""
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-            />
-          </div>
-          <div className="card hover:bg-[#dfe4ea] h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer">
-            <p className="text-[#585858] font-[17px]">
-              Help me find YouTube videos to care for a specific plant
-            </p>
-            <img
-              src={assets.code_icon}
-              alt=""
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-            />
-          </div>
-        </div>
-
-        </>
-        : <div className="result px-[5%] max-h-[70vh] overflow-y-scroll scrollbar-hide">
-          <div className="result-title font-medium text-[17px] my-[40px] flex items-center gap-[26px]">
-            <img className="w-[40px] h-[40px] rounded-full object-cover" src={assets.user_icon} alt="" />
-            <p>{recentPrompt}</p>
-          </div>
-          <div className="result-data flex items-start gap-[20px]">
-            <img className="w-[45px]" src={assets.gemini_icon} alt="" />
-            {loading ? <div className="loader w-full flex flex-col gap-[10px]">
-              <hr className="rounded-[4px] border-0 bg-[#f6f7f8] h-[20px]"/>
-              <hr className="rounded-[4px] border-0 bg-[#f6f7f8] h-[20px]"/>
-              <hr className="rounded-[4px] border-0 bg-[#f6f7f8] h-[20px]"/>
-            </div> : <p className="text-[17px]" dangerouslySetInnerHTML={{__html:resultData}}></p>}
-          </div>
-        </div>
-        }
-
-        
         <div className="main-bottom absolute bottom-0 w-full max-w-[900px] px-[20px] m-auto">
-        <div className="mt-[100px] search-box flex items-center justify-between gap-[20px] bg-[#f0f4f9] py-[10px] px-[20px] rounded-[50px]">
-  <input
-    onChange={(e) => setInput(e.target.value)}
-    value={input}
-    className="flex-[1] bg-transparent border-none p-[8px] text-[18px] outline-none"
-    type="text"
-    placeholder="Enter a prompt here"
-    onKeyDown={(e) => {
-      if (e.key === 'Enter') {
-        handleSend();
-      }
-    }}
-  />
-  <div className="flex gap-2">
-    <img
-      className="w-[24px] cursor-pointer"
-      src={assets.gallery_icon}
-      alt=""
-    />
-    <img
-      className="w-[24px] cursor-pointer"
-      src={assets.mic_icon}
-      alt=""
-    />
-    <img
-      onClick={handleSend}
-      className="w-[24px] cursor-pointer"
-      src={assets.send_icon}
-      alt=""
-    />
-  </div>
-</div>
+          <div className="mt-[100px] search-box flex items-center justify-between gap-[0px] bg-[#1E1F20] py-[10px] px-[20px] rounded-[50px]">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              className="flex-[1] text-[#E3E3E3] bg-transparent border-none p-[8px] text-[18px] outline-none"
+              type="text"
+              placeholder="Enter a prompt here"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSend();
+                }
+              }}
+            />
+            <div className="flex gap-4 items-center">
+            <BiImageAdd
+                size={45}
+                className="text-[#E3E3E3] w-[26px] cursor-pointer"
+              />
+              <IoIosCamera
+                size={48}
+                className="text-[#E3E3E3] w-[26px] cursor-pointer"
+              />
+              <IoMdSend
+              onClick={handleSend}
+                size={42}
+                className="text-[#E3E3E3] w-[26px] cursor-pointer"
+              />
+            </div>
+          </div>
 
-          <p className="bottom-info text-[13px] my-[15px] mx-auto text-center font-light">
-            Gemini may display inaccurate info, including about people, so
-            double-check its responses. Your privacy and Gemini Apps
+          <p className="bottom-info text-[13px] text-[#E3E3E3] my-[15px] mx-auto text-center font-light">
+            Some information given by this clone may not be correct because of free Google - gemini API so think twice before blaming
+            pokkii Nakul.
           </p>
         </div>
       </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,11 @@ import React, { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Sidebar.css";
 import { Context } from "../../context/Context";
+import { IoMenu, IoSettingsSharp } from "react-icons/io5";
+import { MdHistory } from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
+import { IoIosAdd, IoMdHelpCircleOutline } from "react-icons/io";
+import { FiMessageSquare } from "react-icons/fi";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
@@ -13,41 +18,50 @@ const Sidebar = () => {
   };
 
   return (
-    <div id="sidebar" className="sidebar min-h-[100vh] inline-flex flex-col justify-between py-[25px] px-[15px] bg-[#f0f4f9]">
+    <div
+      id="sidebar"
+      className="sidebar min-h-[100vh] inline-flex flex-col justify-between py-[25px] px-[15px] bg-[#1E1F20]"
+    >
       <div className="top">
-        <img
+        <IoMenu
+        size={30}
           onClick={() => setExtended((prev) => !prev)}
-          className="menu block pb-6 ml-[15px] cursor-pointer"
-          src={assets.menu_icon}
-          alt=""
+          className="text-[#E5E5E5] menu block ml-[12px] cursor-pointer"
         />
-        <div onClick={newChat} className="new-chat mt-[10px] inline-flex items-center gap-[10px] py-[10px] px-[15px] bg-[#e6eaf1] rounded-[50px] text-[14px] text-zinc-500 cursor-pointer">
-          <img src={assets.plus_icon} alt="" />
+        <div
+          onClick={newChat}
+          className="new-chat mt-[10px] inline-flex items-center gap-[10px] py-[10px] px-[10px] bg-[#282A2C] rounded-[50px] text-[14px] text-[#E8EAED] cursor-pointer"
+        >
+          <IoIosAdd size={32}/>
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended ? (
           <div className="recent flex flex-col">
-            <p className="recent-title mt-[30px] mb-[20px]">Recent</p>
+            <p className="text-[#E5E5E5] recent-title mt-[30px] mb-[20px]">Recent</p>
             {prevPrompt.map((item, index) => (
-              <div key={index} onClick={() => loadPropt(item)} className="recent-entry flex items-start items-center gap-[10px] p-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#E6EAF1]">
-                <img src={assets.message_icon} alt="" />
-                <p>{item.slice(0, 13)} ...</p>
+              <div
+                key={index}
+                onClick={() => loadPropt(item)}
+                className="recent-entry flex items-start items-center gap-[10px] p-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#282A2C]"
+              >
+                <FiMessageSquare className="text-[#E5E5E5]"/>
+                <p className="text-[#E5E5E5]">{item.slice(0, 13)} ...</p>
               </div>
             ))}
           </div>
         ) : null}
       </div>
       <div className="bottom flex flex-col">
-        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[15px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#E6EAF1] cursor-pointer">
-          <img src={assets.question_icon} alt="" />
+        <div className="text-[#E5E5E5] bottom-item recent-entry flex items-start gap-[10px] p-[15px] pl-[14px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#303131] cursor-pointer">
+          <IoMdHelpCircleOutline size={25} />
           {extended ? <p>Help</p> : null}
         </div>
-        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[15px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#E6EAF1] cursor-pointer">
-          <img src={assets.history_icon} alt="" />
+        <div className="bottom-item text-[#E5E5E5] recent-entry flex items-start gap-[10px] p-[15px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#303131] cursor-pointer">
+          <FaHistory size={21} />
           {extended ? <p>Activity</p> : null}
         </div>
-        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[15px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#E6EAF1] cursor-pointer">
-          <img src={assets.setting_icon} alt="" />
+        <div className="bottom-item text-[#E5E5E5] recent-entry flex items-start gap-[10px] p-[15px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#303131] cursor-pointer">
+          <IoSettingsSharp size={21} />
           {extended ? <p>Settings</p> : null}
         </div>
       </div>
